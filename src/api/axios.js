af83379f2bdfb3bdd20a51a8006914b2ebf107de
Timothy Ku <timothywanjiru1@gmail.com>
2025-04-21 17:54:23 +0300

    edit weather.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,9 +3,9 @@ import axios from "axios";
 //const instance = axios.create({
   //baseURL: "http://localhost:5000/api", // DO NOT add `/routine` here
 //});
-
 const instance = axios.create({
-  baseURL: "beauty-bliss-server-production.up.railway.app", // DO NOT add `/routine` here
+  baseURL: "https://beauty-bliss-server-production.up.railway.app/",
 });
+
 
 export default instance;
